76f7aad40a69a0a19e9704e4ce761fff58a8fe99
Hella <krisbeaky@gmail.com>
2021-10-31 15:59:40 +0500

    Add some beaty and inheritance

--- blog/views.py
+++ blog/views.py
@@ -6,23 +6,23 @@ posts = [
         'author': 'CoreyMS',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_published': 'August 27, 2018',
+        'date_posted': 'August 27, 2018',
     },
     {
         'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_published': 'August 28, 2018',
+        'date_posted': 'August 28, 2018',
     }
 ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': posts,
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
