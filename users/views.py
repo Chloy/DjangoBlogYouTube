ca185798f581b76633b488c643fc3bae651c873b
Hella <krisbeaky@gmail.com>
2021-11-03 23:13:11 +0500

    Add login, logoutand profile pages

--- users/views.py
+++ users/views.py
@@ -10,8 +10,11 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created!')
+            return redirect('login')
     else:
-        form = UserRegisterForm()
+        form = UserRegisterForm ()
     return render(request, 'users/register.html', {'form': form})
+
+def profile(request):
+    return render(request, 'users/profile.html')
